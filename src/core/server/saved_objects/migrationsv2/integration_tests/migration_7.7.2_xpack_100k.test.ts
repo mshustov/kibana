ce2cdde939a2728e63d8c7a6c50070a9cbf5fd49
restrry <restrry@gmail.com>
2021-04-09 15:04:12 +0200

    add a test to migrate 100k with hash-based id generation in es

--- src/core/server/saved_objects/migrationsv2/integration_tests/migration_7.7.2_xpack_100k.test.ts
+++ src/core/server/saved_objects/migrationsv2/integration_tests/migration_7.7.2_xpack_100k.test.ts
@@ -17,7 +17,7 @@ import { Root } from '../../../root';
 
 const kibanaVersion = Env.createDefault(REPO_ROOT, getEnvOptions()).packageInfo.version;
 
-describe.skip('migration from 7.7.2-xpack with 100k objects', () => {
+describe('migration from 7.7.2-xpack with 100k objects', () => {
   let esServer: kbnTestServer.TestElasticsearchUtils;
   let root: Root;
   let coreStart: InternalCoreStart;
@@ -104,15 +104,73 @@ describe.skip('migration from 7.7.2-xpack with 100k objects', () => {
   });
 
   it('copies all the document of the previous index to the new one', async () => {
-    const migratedIndexResponse = await esClient.count({
-      index: migratedIndex,
+    console.time('createPipeline');
+    /**
+     * We use ingest pipeline to create a script since it's the only API
+     * when painless provides access to hashing functions sha1 and sha256
+     * https://github.com/elastic/elasticsearch/issues/61244#issuecomment-675514000
+     * examples:
+     * https://github.com/elastic/elasticsearch/pull/59671#issuecomment-659467724
+     * https://github.com/elastic/elasticsearch/pull/63375/files#diff-2f417b71ec83e11b4254559f37098b86a111298279bf600332b17d376b0a8357
+     * but in theory it can have inconsistent order of keys https://github.com/elastic/elasticsearch/issues/34085
+     *
+     * Other alternatives:
+     * - use base64 https://github.com/elastic/elasticsearch/pull/22665 creates a long id, not practical
+     * - use _source.hashCode can produce inconsistent result on different machines https://eclipsesource.com/blogs/2012/09/04/the-3-things-you-should-know-about-hashcode/
+     */
+    const response = esClient.ingest.putPipeline({
+      id: 'id-hash-generator',
+      body: {
+        processors: [
+          {
+            script: {
+              // available Painless API https://www.elastic.co/guide/en/elasticsearch/painless/master/painless-api-reference-shared-java-lang.html
+              lang: 'painless',
+              /**
+               * Need more work to implement it properly. Right now, the result is not idempotent
+               * because pipeline context includes dynamic fields created during ingestion.
+               * We need to find a way to filter them out to use the document for hashing.
+               * For more details about ingestion context, see
+               * https://www.elastic.co/guide/en/elasticsearch/painless/master/painless-ingest-processor-context.html
+               */
+              source: 'ctx._id = ctx.toString().sha256()',
+            },
+          },
+        ],
+      },
     });
+    console.timeEnd('createPipeline');
+    console.log('@@@createPipeline response', response);
+
+    console.time('reindexES');
+    const reindexDest = 'migration_es_dest';
+    /**
+     * Run reindex with 'id-hash-generator' pipeline to create idempotent _id based on _source HashMap.
+     */
+    const reindexResponse = await esClient.reindex({
+      wait_for_completion: true,
+      body: {
+        source: { index: migratedIndex },
+        dest: {
+          index: reindexDest,
+          pipeline: 'id-hash-generator',
+        },
+      },
+    });
+    console.timeEnd('reindexES');
+    console.log('@@@reindexES response', reindexResponse);
+
+    const migratedIndexResponse = await esClient.count({
+      index: reindexDest,
+    });
+    // const migratedIndexResponse = await esClient.count({
+    //   index: migratedIndex,
+    // });
+    //
     const oldIndexResponse = await esClient.count({
       index: '.kibana_1',
     });
 
-    // Use a >= comparison since once Kibana has started it might create new
-    // documents like telemetry tasks
     expect(migratedIndexResponse.body.count).toBeGreaterThanOrEqual(oldIndexResponse.body.count);
   });
 });
